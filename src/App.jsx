218ceb45c3c367b20d278eadaf93244886c3e709
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-23 21:16:35 +0200

    cleaning app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className='App'>
-      <header className='App-header'>Template React Vite</header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
